a3a9b87e3a6cd26b19ce5d602e6bc746cc055e9f
Shiyan Xu <2701446+xushiyan@users.noreply.github.com>
2024-07-02 22:08:08 -0500

    refactor: improve error handling in storage module

--- crates/core/src/storage/mod.rs
+++ crates/core/src/storage/mod.rs
@@ -21,7 +21,8 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
+use arrow::compute::concat_batches;
 use arrow::record_batch::RecordBatch;
 use async_recursion::async_recursion;
 use bytes::Bytes;
@@ -60,16 +61,21 @@ impl Storage {
         }
     }
 
-    #[allow(dead_code)]
-    pub async fn get_file_info(&self, relative_path: &str) -> FileInfo {
-        let obj_url = join_url_segments(&self.base_url, &[relative_path]).unwrap();
-        let obj_path = ObjPath::from_url_path(obj_url.path()).unwrap();
-        let meta = self.object_store.head(&obj_path).await.unwrap();
-        FileInfo {
-            uri: obj_url.to_string(),
-            name: obj_path.filename().unwrap().to_string(),
+    #[cfg(test)]
+    async fn get_file_info(&self, relative_path: &str) -> Result<FileInfo> {
+        let obj_url = join_url_segments(&self.base_url, &[relative_path])?;
+        let obj_path = ObjPath::from_url_path(obj_url.path())?;
+        let meta = self.object_store.head(&obj_path).await?;
+        let uri = obj_url.to_string();
+        let name = obj_path
+            .filename()
+            .ok_or(anyhow!("Failed to get file name for {}", obj_path))?
+            .to_string();
+        Ok(FileInfo {
+            uri,
+            name,
             size: meta.size,
-        }
+        })
     }
 
     pub async fn get_parquet_file_metadata(&self, relative_path: &str) -> Result<ParquetMetaData> {
@@ -79,79 +85,100 @@ impl Storage {
         let meta = obj_store.head(&obj_path).await?;
         let reader = ParquetObjectReader::new(obj_store, meta);
         let builder = ParquetRecordBatchStreamBuilder::new(reader).await?;
-        Ok(builder.metadata().as_ref().to_owned())
+        Ok(builder.metadata().as_ref().clone())
     }
 
-    pub async fn get_file_data(&self, relative_path: &str) -> Bytes {
-        let obj_url = join_url_segments(&self.base_url, &[relative_path]).unwrap();
-        let obj_path = ObjPath::from_url_path(obj_url.path()).unwrap();
-        let result = self.object_store.get(&obj_path).await.unwrap();
-        result.bytes().await.unwrap()
+    pub async fn get_file_data(&self, relative_path: &str) -> Result<Bytes> {
+        let obj_url = join_url_segments(&self.base_url, &[relative_path])?;
+        let obj_path = ObjPath::from_url_path(obj_url.path())?;
+        let result = self.object_store.get(&obj_path).await?;
+        let bytes = result.bytes().await?;
+        Ok(bytes)
     }
 
-    pub async fn get_parquet_file_data(&self, relative_path: &str) -> Vec<RecordBatch> {
-        let obj_url = join_url_segments(&self.base_url, &[relative_path]).unwrap();
-        let obj_path = ObjPath::from_url_path(obj_url.path()).unwrap();
+    pub async fn get_parquet_file_data(&self, relative_path: &str) -> Result<RecordBatch> {
+        let obj_url = join_url_segments(&self.base_url, &[relative_path])?;
+        let obj_path = ObjPath::from_url_path(obj_url.path())?;
         let obj_store = self.object_store.clone();
-        let meta = obj_store.head(&obj_path).await.unwrap();
+        let meta = obj_store.head(&obj_path).await?;
+
+        // read parquet
         let reader = ParquetObjectReader::new(obj_store, meta);
-        let stream = ParquetRecordBatchStreamBuilder::new(reader)
-            .await
-            .unwrap()
-            .build()
-            .unwrap();
-        stream
-            .collect::<Vec<_>>()
-            .await
-            .into_iter()
-            .map(|r| r.unwrap())
-            .collect()
+        let builder = ParquetRecordBatchStreamBuilder::new(reader).await?;
+        let schema = builder.schema().clone();
+        let mut stream = builder.build()?;
+        let mut batches = Vec::new();
+
+        while let Some(r) = stream.next().await {
+            let batch = r.context("Failed to read record batch.")?;
+            batches.push(batch)
+        }
+
+        if batches.is_empty() {
+            return Ok(RecordBatch::new_empty(schema.clone()));
+        }
+
+        concat_batches(&schema, &batches)
+            .map_err(|e| anyhow!("Failed to concat record batches: {}", e))
     }
 
-    pub async fn list_dirs(&self, subdir: Option<&str>) -> Vec<String> {
-        self.list_dirs_as_obj_paths(subdir)
-            .await
-            .into_iter()
-            .map(|p| p.filename().unwrap().to_string())
-            .collect()
-    }
-
-    async fn list_dirs_as_obj_paths(&self, subdir: Option<&str>) -> Vec<ObjPath> {
-        let prefix_url = join_url_segments(&self.base_url, &[subdir.unwrap_or_default()]).unwrap();
-        let prefix_path = ObjPath::from_url_path(prefix_url.path()).unwrap();
-        self.object_store
-            .list_with_delimiter(Some(&prefix_path))
-            .await
-            .unwrap()
-            .common_prefixes
-    }
-
-    pub async fn list_files(&self, subdir: Option<&str>) -> Vec<FileInfo> {
-        let prefix_url = join_url_segments(&self.base_url, &[subdir.unwrap_or_default()]).unwrap();
-        let prefix_path = ObjPath::from_url_path(prefix_url.path()).unwrap();
-        self.object_store
-            .list_with_delimiter(Some(&prefix_path))
-            .await
-            .unwrap()
-            .objects
-            .into_iter()
-            .map(|obj_meta| FileInfo {
-                uri: join_url_segments(&prefix_url, &[obj_meta.location.filename().unwrap()])
-                    .unwrap()
+    pub async fn list_dirs(&self, subdir: Option<&str>) -> Result<Vec<String>> {
+        let dir_paths = self.list_dirs_as_obj_paths(subdir).await?;
+        let mut dirs = Vec::new();
+        for dir in dir_paths {
+            dirs.push(
+                dir.filename()
+                    .ok_or(anyhow!("Failed to get file name for {}", dir))?
                     .to_string(),
-                name: obj_meta.location.filename().unwrap().to_string(),
+            )
+        }
+        Ok(dirs)
+    }
+
+    async fn list_dirs_as_obj_paths(&self, subdir: Option<&str>) -> Result<Vec<ObjPath>> {
+        let prefix_url = join_url_segments(&self.base_url, &[subdir.unwrap_or_default()])?;
+        let prefix_path = ObjPath::from_url_path(prefix_url.path())?;
+        let list_res = self
+            .object_store
+            .list_with_delimiter(Some(&prefix_path))
+            .await?;
+        Ok(list_res.common_prefixes)
+    }
+
+    pub async fn list_files(&self, subdir: Option<&str>) -> Result<Vec<FileInfo>> {
+        let prefix_url = join_url_segments(&self.base_url, &[subdir.unwrap_or_default()])?;
+        let prefix_path = ObjPath::from_url_path(prefix_url.path())?;
+        let list_res = self
+            .object_store
+            .list_with_delimiter(Some(&prefix_path))
+            .await?;
+        let mut file_info = Vec::new();
+        for obj_meta in list_res.objects {
+            let name = obj_meta
+                .location
+                .filename()
+                .ok_or(anyhow!(
+                    "Failed to get file name for {:?}",
+                    obj_meta.location
+                ))?
+                .to_string();
+            let uri = join_url_segments(&prefix_url, &[&name])?.to_string();
+            file_info.push(FileInfo {
+                uri,
+                name,
                 size: obj_meta.size,
-            })
-            .collect()
+            });
+        }
+        Ok(file_info)
     }
 }
 
 #[async_recursion]
-pub async fn get_leaf_dirs(storage: &Storage, subdir: Option<&str>) -> Vec<String> {
+pub async fn get_leaf_dirs(storage: &Storage, subdir: Option<&str>) -> Result<Vec<String>> {
     let mut leaf_dirs = Vec::new();
-    let child_dirs = storage.list_dirs(subdir).await;
+    let child_dirs = storage.list_dirs(subdir).await?;
     if child_dirs.is_empty() {
-        leaf_dirs.push(subdir.unwrap().to_owned());
+        leaf_dirs.push(subdir.unwrap_or_default().to_owned());
     } else {
         for child_dir in child_dirs {
             let mut next_subdir = PathBuf::new();
@@ -159,11 +186,14 @@ pub async fn get_leaf_dirs(storage: &Storage, subdir: Option<&str>) -> Vec<Strin
                 next_subdir.push(curr);
             }
             next_subdir.push(child_dir);
-            let curr_leaf_dir = get_leaf_dirs(storage, Some(next_subdir.to_str().unwrap())).await;
+            let next_subdir = next_subdir
+                .to_str()
+                .ok_or(anyhow!("Failed to convert path: {:?}", next_subdir))?;
+            let curr_leaf_dir = get_leaf_dirs(storage, Some(next_subdir)).await?;
             leaf_dirs.extend(curr_leaf_dir);
         }
     }
-    leaf_dirs
+    Ok(leaf_dirs)
 }
 
 #[cfg(test)]
@@ -187,7 +217,8 @@ mod tests {
         )
         .unwrap();
         let storage = Storage::new(Arc::new(base_url), Arc::new(HashMap::new())).unwrap();
-        let first_level_dirs: HashSet<String> = storage.list_dirs(None).await.into_iter().collect();
+        let first_level_dirs: HashSet<String> =
+            storage.list_dirs(None).await.unwrap().into_iter().collect();
         assert_eq!(
             first_level_dirs,
             vec![".hoodie", "part1", "part2", "part3"]
@@ -195,9 +226,9 @@ mod tests {
                 .map(String::from)
                 .collect()
         );
-        let second_level_dirs: Vec<String> = storage.list_dirs(Some("part2")).await;
+        let second_level_dirs: Vec<String> = storage.list_dirs(Some("part2")).await.unwrap();
         assert_eq!(second_level_dirs, vec!["part22"]);
-        let no_dirs = storage.list_dirs(Some("part1")).await;
+        let no_dirs = storage.list_dirs(Some("part1")).await.unwrap();
         assert!(no_dirs.is_empty());
     }
 
@@ -211,6 +242,7 @@ mod tests {
         let first_level_dirs: HashSet<ObjPath> = storage
             .list_dirs_as_obj_paths(None)
             .await
+            .unwrap()
             .into_iter()
             .collect();
         let expected_paths: HashSet<ObjPath> = vec![".hoodie", "part1", "part2", "part3"]
@@ -230,7 +262,12 @@ mod tests {
         )
         .unwrap();
         let storage = Storage::new(Arc::new(base_url), Arc::new(HashMap::new())).unwrap();
-        let file_info_1: Vec<FileInfo> = storage.list_files(None).await.into_iter().collect();
+        let file_info_1: Vec<FileInfo> = storage
+            .list_files(None)
+            .await
+            .unwrap()
+            .into_iter()
+            .collect();
         assert_eq!(
             file_info_1,
             vec![FileInfo {
@@ -242,6 +279,7 @@ mod tests {
         let file_info_2: Vec<FileInfo> = storage
             .list_files(Some("part1"))
             .await
+            .unwrap()
             .into_iter()
             .collect();
         assert_eq!(
@@ -259,6 +297,7 @@ mod tests {
         let file_info_3: Vec<FileInfo> = storage
             .list_files(Some("part2/part22"))
             .await
+            .unwrap()
             .into_iter()
             .collect();
         assert_eq!(
@@ -282,10 +321,24 @@ mod tests {
         )
         .unwrap();
         let storage = Storage::new(Arc::new(base_url), Arc::new(HashMap::new())).unwrap();
-        let leaf_dirs = get_leaf_dirs(&storage, None).await;
+        let leaf_dirs = get_leaf_dirs(&storage, None).await.unwrap();
         assert_eq!(
             leaf_dirs,
             vec![".hoodie", "part1", "part2/part22", "part3/part32/part33"]
+        );
+    }
+
+    #[tokio::test]
+    async fn use_storage_to_get_leaf_dirs_for_leaf_dir() {
+        let base_url =
+            Url::from_directory_path(canonicalize(Path::new("fixtures/leaf_dir")).unwrap())
+                .unwrap();
+        let storage = Storage::new(Arc::new(base_url), Arc::new(HashMap::new())).unwrap();
+        let leaf_dirs = get_leaf_dirs(&storage, None).await.unwrap();
+        assert_eq!(
+            leaf_dirs,
+            vec![""],
+            "Listing a leaf dir should get the relative path to itself."
         );
     }
 
@@ -294,7 +347,7 @@ mod tests {
         let base_url =
             Url::from_directory_path(canonicalize(Path::new("fixtures")).unwrap()).unwrap();
         let storage = Storage::new(Arc::new(base_url), Arc::new(HashMap::new())).unwrap();
-        let file_info = storage.get_file_info("a.parquet").await;
+        let file_info = storage.get_file_info("a.parquet").await.unwrap();
         assert_eq!(file_info.name, "a.parquet");
         assert_eq!(
             file_info.uri,
@@ -308,8 +361,7 @@ mod tests {
         let base_url =
             Url::from_directory_path(canonicalize(Path::new("fixtures")).unwrap()).unwrap();
         let storage = Storage::new(Arc::new(base_url), Arc::new(HashMap::new())).unwrap();
-        let file_data = storage.get_parquet_file_data("a.parquet").await;
-        assert_eq!(file_data.len(), 1);
-        assert_eq!(file_data.first().unwrap().num_rows(), 5);
+        let file_data = storage.get_parquet_file_data("a.parquet").await.unwrap();
+        assert_eq!(file_data.num_rows(), 5);
     }
 }
