ff221f987b9e4ca751480bfcf50ef0d70da40714
Shiyan Xu <2701446+xushiyan@users.noreply.github.com>
2025-06-26 15:09:24 -0500

    add tests

--- crates/core/src/schema/mod.rs
+++ crates/core/src/schema/mod.rs
@@ -33,3 +33,32 @@ pub fn prepend_meta_fields_with_operation(schema: SchemaRef) -> Result<Schema> {
     Schema::try_merge([meta_field_schema.as_ref().clone(), schema.as_ref().clone()])
         .map_err(CoreError::ArrowError)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use arrow_schema::{DataType, Field};
+    use std::sync::Arc;
+
+    #[test]
+    fn test_prepend_meta_fields() {
+        let schema = Schema::new(vec![Field::new("field1", DataType::Int32, false)]);
+        let new_schema = prepend_meta_fields(Arc::new(schema)).unwrap();
+        assert_eq!(new_schema.fields().len(), 6);
+
+        let field_names: Vec<_> = new_schema.fields().iter().map(|f| f.name()).collect();
+        assert_eq!(field_names[..5], MetaField::field_names());
+        assert_eq!(field_names[5], "field1");
+    }
+
+    #[test]
+    fn test_prepend_meta_fields_with_operation() {
+        let schema = Schema::new(vec![Field::new("field1", DataType::Int32, false)]);
+        let new_schema = prepend_meta_fields_with_operation(Arc::new(schema)).unwrap();
+        assert_eq!(new_schema.fields().len(), 7);
+
+        let field_names: Vec<_> = new_schema.fields().iter().map(|f| f.name()).collect();
+        assert_eq!(field_names[..6], MetaField::field_names_with_operation());
+        assert_eq!(field_names[6], "field1");
+    }
+}
